7834efd96df6f5ae61c9b2198c1614da2f481610
Tanawat Limthongkul <113848137+Tanawatlim@users.noreply.github.com>
2023-04-05 16:26:06 +0700

    Update Navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,59 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
 import Image from "next/image";
 import logo from "../../../public/logo.png";
 import ProfileImg from "../../../public/Profile.png";
 import Search from "./Search";
 import Link from "next/link";
 import NavLink from "./NavLink";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 interface props {
   children: JSX.Element;
 }
 
 const Navbar = ({ children }: props) => {
+  const [toggle, setToggle] = React.useState(false);
+  const toggleDropdown = () => {
+    setToggle(!toggle);
+  };
   return (
-    // <div className="z-10 grid grid-cols-6 border-b border-F5F5F5 bg-242527 p-2 sticky top-0">
-    //   <Link className="col-start-1 col-end-3 grid grid-cols-3" href={"/home"}>
-    //     <Image src={logo} alt="" width={90} height={90} />
-    //     <button className="bg-white hover:bg-F5F5F5 w-11/12 h-full text-black border border-white border-2 focus:border-b-21F5E6">
-    //       หน้าหลัก
-    //     </button>
-    //     <button className="bg-white hover:bg-F5F5F5 w-11/12 h-full text-black border border-white border-2 focus:border-b-21F5E6">
-    //       อันดับ
-    //     </button>
-    //   </Link>
-
-    //   <div className="col-start-3 col-end-6">
-    //     <Search />
-    //   </div>
-
-    //   <div className="col-start-6 col-end-6 flex justify-end">
-    //     <Link
-    //       className="grid grid-cols-2 justify-items-stretch flex items-stretch"
-    //       href={"/login"}
-    //     >
-    //       <label className="self-center text-black">Thanasini</label>
-    //       <Image
-    //         className="justify-self-end"
-    //         src={ProfileImg}
-    //         alt=""
-    //         width={50}
-    //         height={50}
-    //         priority
-    //       />
-    //     </Link>
-    //   </div>
-    // </div>
-
     <>
       <nav className="flex justify-around items-center sticky top-0 p-2 bg-F5F5F5 drop-shadow">
         {/* Nav Brand */}
         <Link href="/home">
-          <Image src={logo} alt="" width={90} height={90} />
+          <Image src={logo} alt="" width={60} height={60} />
         </Link>
 
         {/* Nav Links */}
-        <ul>
+        <ul className="max-lg:hidden">
           <NavLink path="/home" name="หน้าหลัก" />
           <NavLink path="/home" name="อันดับ" />
         </ul>
@@ -63,9 +35,21 @@ const Navbar = ({ children }: props) => {
           <Search />
         </div>
 
+        <button className="lg:hidden relative" onClick={() => toggleDropdown()}>
+          <RxHamburgerMenu />
+          {toggle && (
+            <ul className="flex flex-col absolute top-5 right-0 bg-F5F5F5 border">
+              <NavLink path="/home" name="หน้าหลัก" />
+              <NavLink path="/home" name="อันดับ" />
+            </ul>
+          )}
+        </button>
+
         {/* User Account */}
         <div className="flex">
-          <label className="self-center text-black mx-3">Thanasini</label>
+          <label className="max-lg:hidden self-center text-black mx-3">
+            Thanasini
+          </label>
           <Link
             className="justify-items-stretch flex items-stretch"
             href={"/login"}
---
+++ src/pages/artistprofile/index.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/navbar";
+import React from "react";
+
+const index = () => {
+  return (
+    <Navbar>
+      <div>index</div>
+    </Navbar>
+  );
+};
+
+export default index;
